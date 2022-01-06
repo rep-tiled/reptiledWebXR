447b5784636fbdbaa577f1c45bd4935f8705e70e
alizardguy <42082829+alizardguy@users.noreply.github.com>
2022-01-05 17:46:25 -0800

    vr stuff

---
+++ scripts/colorToggle.js
@@ -0,0 +1,15 @@
+AFRAME.registerComponent('color-toggle', {
+    init: function(){
+        let el =this.el;
+        this.toggleColor = function(){
+            el.setAttribute('color','black');
+        }
+        this.el.addEventListener('click', this.toggleColor); //listen for click event
+    },
+
+    remove: function(){
+        this.el.removeEventListener('click', this.toggleColor) //cleanup if component removed
+    }
+
+
+})
